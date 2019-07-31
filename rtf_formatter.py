907633952854967d8b3b7b1833dd38a379b87080
Midlight25 <mannolaur@gmail.com>
2019-07-30 20:20:56 -0400

    GI and PY file update, first time it worked. Only works with existing files.

--- rtf_formatter.py
+++ rtf_formatter.py
@@ -1,3 +1,19 @@
-with open("test.rtf") as rtf_file:
-    text_data = rtf_file.read()
-    print(text_data)
+import os
+
+this_folder = os.path.dirname(os.path.abspath(__file__))
+the_file = os.path.join(this_folder, "test.rtf")
+
+
+with open(the_file) as file:
+    text_data = file.readlines()
+
+new_file_lines = []
+
+for line in text_data:
+    new_file_lines.append(line.replace("\line", "\par"))
+
+new_file = os.path.join(this_folder, "new_test.rtf")
+
+with open(new_file, "w") as file:
+    for line in new_file_lines:
+        file.write(line)
