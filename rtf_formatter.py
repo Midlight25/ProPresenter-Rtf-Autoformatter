2794d026b516a98adeb597234631083b82dc1cb1
Midlight25 <mannolaur@gmail.com>
2019-08-02 19:40:34 -0400

    Added simple CLI to program, activated when run with no arguments.

--- rtf_formatter.py
+++ rtf_formatter.py
@@ -13,32 +13,32 @@ def auto_format_rtf(file_path):
 
     # Verifies that file exists and is .rtf before starting
     if os.path.exists(file_path) and file_ext == ".rtf":
-        # print("Checks passed, beginning process.")
-        # print("Modifiying {file_name}{file_ext}.".format(file_name=file_name, file_ext=file_ext))
+        print("Checks passed on {file_path}, beginning process.".format(file_path=file_path))
+        print("Modifiying {file_name}{file_ext}.".format(file_name=file_name, file_ext=file_ext))
 
         # Finds file directory from file path and changes to it.
         file_location = os.path.dirname(file_path)
         os.chdir(file_location)
-        # print("Active directory changed to {file_location}.".format(file_location=file_location))
+        print("Active directory changed to {file_location}.".format(file_location=file_location))
 
         # Opens file and copies data to text_data.
         with open(file_path) as file:
             text_data = file.read()
-        # print("Opened file and read data to text_data.")
+        print("Opened file and read data to text_data.")
 
         # Formats data and adds it to list for appending.
         new_text_data = text_data.replace("\line", "\par")
-        # print("Formatted data")
+        print("Formatted data")
 
         # Creates new file name and path from original file data.
         new_file_name = file_name + " MODIFIED" + file_ext
         new_file = os.path.join(file_location, new_file_name)
-        # print("Created new file name, new file at {new_file}".format(new_file=new_file))
+        print("Created new file name, new file at {new_file}".format(new_file=new_file))
 
         # Writes data to new file
         with open(new_file, "w+") as file:
             file.write(new_text_data)
-        # print("Wrote data to \"{new_file_name}\".".format(new_file_name=new_file_name))
+        print("Wrote data to \"{new_file_name}\".\n".format(new_file_name=new_file_name))
     return new_file
 
 
@@ -47,17 +47,35 @@ if __name__ == '__main__':
     if len(sys.argv) > 1:
         for arg in sys.argv[1:]:
             if os.path.exists(arg):
-
                 sys.stdout.write("Modifiying file {filename}.\n".format(filename=arg))
                 new_file_path = auto_format_rtf(arg)
+                sys.stdout.flush()
 
                 if os.path.exists(new_file_path):
                     sys.stdout.write("New file created @ {file_path}.\n".format(
                         file_path=new_file_path))
+                    sys.stdout.flush()
                 else:
                     sys.stderr.write("Error creating new file.\n")
+                    sys.stdout.flush()
             else:
                 sys.stdout.write(
                     "{file_path} does not exist, file not created.".format(file_path=arg))
+                sys.stdout.flush()
 
-    print("test")
+    else:
+        sys.stdout.write("\nProPresenter RTF Autoformatter © Midlight25 2019\n\n")
+        sys.stdout.flush()
+        crashed = False
+        acceptable_exit_answers = ["quit", "q", "Q", "Quit"]
+        while not crashed == True:
+            file_path_get = input(
+                "Please paste the full path of the file you want to modify or write \"quit\" to exit the program:\n")
+            if file_path_get in acceptable_exit_answers:
+                sys.exit("Program exited by user")
+            try:
+                auto_format_rtf(file_path_get)
+            except:
+                sys.stderr.write("Program was unable to create new file, please try again.")
+                sys.stderr.flush()
+        sys.exit("System crashed.")
